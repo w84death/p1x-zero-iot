9283291a4233c1fa5c253de23d388dad6c26f84a
w84death <w84death@gmail.com>
2016-09-23 23:16:30 +0200

    camera preview

--- p1x-zero-iot.py
+++ p1x-zero-iot.py
@@ -5,8 +5,11 @@
 # IMPOTS
 #
 import time
+from time import sleep
+import picamera
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
+import PIL
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -16,6 +19,10 @@ from PIL import ImageFont
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=7, i2c_address=0x3D)
 disp.begin()
 
+# CAM
+#
+camera = picamera.PiCamera()
+camera.resolution = (640,480)
 
 # VARS
 #
@@ -31,6 +38,17 @@ draw.rectangle((0,0,W-1,H-1), outline=255, fill=0)
 draw.text((4, 2),'P1X Zero IoT v0.1',  font=font, fill=255)
 draw.line((2, 18, W-3, 18), fill=255)
 
+draw.text((14, 32),'Boooo!',  font=font, fill=255)
+
+
 disp.image(image)
 disp.display()
 
+while True:
+	camera.capture('swap.jpg')
+	image = Image.open('swap.jpg').rotate(270)
+	image = image.resize((W, H), PIL.Image.NEAREST)
+	image = image.convert('1')
+	disp.image(image)
+	disp.display()
+	#sleep(1)
